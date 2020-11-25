7c839806804e88e2356495bc79d164349fc0a470
Carles-Bordas <carles.bordas@hotmail.com>
2020-11-25 20:22:50 +0100

    Modified travelling salesman solver to take a collection instead of a set

--- src/main/java/com/snowy/travsalesman/geneticalg/TravellingSalesmanSolver.java
+++ src/main/java/com/snowy/travsalesman/geneticalg/TravellingSalesmanSolver.java
@@ -1,7 +1,8 @@
 package com.snowy.travsalesman.geneticalg;
 
+import java.util.Collection;
 import java.util.Set;
 
 public interface TravellingSalesmanSolver {
-    public Route solve(Set<City> cities, City initial);
+    public Route solve(Collection<City> cities, City initial);
 }
